cac4130750f743157eb08ac8351cb930e2070d0e
VictorCubas <vhcubas91@gmail.com>
2020-04-10 17:57:40 -0400

    Fixed day-selected to show

--- Calendar.py
+++ Calendar.py
@@ -8,23 +8,54 @@ else:
     import tkinter as tk
  
 class Calendar:
-    def __init__(self, parent):
+    AMOUNT_WIDGET_PREVIUS = 9
+    def __init__(self, parent, fecha_seleccionado):
         self.values = None
         self.parent = parent
         self.cal = calendar.TextCalendar(calendar.SUNDAY)
-        self.year = datetime.date.today().year
-        self.month = datetime.date.today().month
         self.wid = []
-        self.day_selected = datetime.date.today().day
-        self.month_selected = self.month
-        self.year_selected = self.year
+
+        self.date_init(fecha_seleccionado)
         self.day_name = ''
         self.setup(self.year, self.month)
+        self.mes_anterior_selected = 0
+        self.paint_day_selected(fecha_seleccionado)
+
+    def paint_day_selected(self, fecha_seleccionado):
+        #marcamos el dia selecionado en caso de que ya se haya seleccionado uno
+        if fecha_seleccionado is not None:
+            #calculamos el numero de widget el cual se usa como posicion el el vector wid y asi lo pintamos
+            position = self.day_selected + Calendar.AMOUNT_WIDGET_PREVIUS
+            #procedemos a pintarlo
+            self.wid[position].config(font=('tahoma', '9', 'bold'), fg='#48C2FA', activeforeground='#48C2FA')
+            #guardamos la posicion anterior del widget pintado
+            self.position_anterior = position
+            #guardamos mes anterior
+            self.mes_anterior_selected = self.month
+        else:
+            self.position_anterior = -1
+
+    def date_init(self, fecha_seleccionado):
+        if fecha_seleccionado is not None:
+            self.month = fecha_seleccionado.month
+            self.year = fecha_seleccionado.year
+
+            self.day_selected = fecha_seleccionado.day
+            self.month_selected = self.month
+            self.year_selected = self.year
+
+        else:
+            self.year = datetime.date.today().year
+            self.month = datetime.date.today().month
+
+            self.day_selected = datetime.date.today().day
+            self.month_selected = self.month
+            self.year_selected = self.year
 
     def clear(self):
         for w in self.wid[:]:
             w.grid_forget()
-            #w.destroy()
+
             self.wid.remove(w)
      
     def go_prev(self):
@@ -33,26 +64,35 @@ class Calendar:
         else:
             self.month = 12
             self.year -= 1
-        #self.selected = (self.month, self.year)
+
         self.clear()
         self.setup(self.year, self.month)
+
+        #comparamos en caso de que volvamos al mes anterior de forma que se muestre el dia ya seleccionado
+        if self.month == self.mes_anterior_selected:
+            self.wid[self.position_anterior].config(font=('tahoma', '9', 'bold'), fg='#48C2FA', activeforeground='#48C2FA')
  
     def go_next(self):
+
         if self.month < 12:
             self.month += 1
         else:
             self.month = 1
             self.year += 1
-         
-        #self.selected = (self.month, self.year)
+
         self.clear()
         self.setup(self.year, self.month)
+
+        #comparamos en caso de que volvamos al mes anterior de forma que se muestre el dia ya seleccionado
+        if self.month == self.mes_anterior_selected:
+            self.wid[self.position_anterior].config(font=('tahoma', '9', 'bold'), fg='#48C2FA', activeforeground='#48C2FA')
 
     def selection(self, day, name, position, d):
         self.day_selected = day
         self.month_selected = self.month
         self.year_selected = self.year
         self.day_name = name
+        self.mes_anterior_selected = self.month
          
         #data
         self.values = datetime.date(self.year_selected, self.month_selected, self.day_selected)
@@ -63,7 +103,7 @@ class Calendar:
         self.position_anterior = position
 
     def setup(self, year, m):
-        self.position_anterior = -1
+        #self.position_anterior = -1
         position = 0
         left = tk.Button(self.parent, bg='#F9F9F9', text='<', command=self.go_prev)
         self.wid.append(left)
@@ -93,9 +133,11 @@ class Calendar:
                     position += 1
                     b = tk.Button(self.parent, width=1, bg='#F9F9F9', text=day, command=lambda day=day, position=position, d=d:self.selection(day, calendar.day_name[(d-1) % 7], position, d))
                     #b = tk.Button(self.parent, width=1, bg='#F9F9F9', text=day, command=lambda day=day, position=position,:self.selection(day, calendar.day_name[(d-1) % 7], position))
+
+                    #if position == self.position_anterior:
                     self.wid.append(b)
                     b.grid(row=w, column=d)
-         
+
     def kill_and_save(self):
         self.parent.destroy()
 
