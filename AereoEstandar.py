cc9265867132970e6546002aebfa7d31b2702aca
VictorCubas <vhcubas91@gmail.com>
2020-04-23 14:59:28 -0400

    Fixed

--- AereoEstandar.py
+++ AereoEstandar.py
@@ -9,14 +9,8 @@ class AereoEstandar(Aereo):
 	cantidad_total_aereo_estandar = 0
 	TIPO = 'Estandar'
 
-	#def __init__(self, precio, senha, fecha_de_viaje, **kwargs):
 	def __init__(self, precio, senha, **kwargs):
 		super().__init__(precio, senha, **kwargs)
-		#self.precio = precio
-		#self.saldo = precio
-		#self.senha = senha
-		#self.fecha_de_viaje = fecha_de_viaje
-
 		#Incrementamos la cantidad de instancias
 		AereoEstandar.cantidad_total_aereo_estandar += 1
 
