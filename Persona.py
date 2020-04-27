d367cbdcdbc2b30e59513db548bd07160dcfd44b
VictorCubas <vhcubas91@gmail.com>
2020-04-27 16:36:17 -0400

    Fixed atribute: contactos[]

--- Persona.py
+++ Persona.py
@@ -13,11 +13,29 @@ class Persona:
 		self.ci = ci
 		self.fecha_nacimiento = fecha_nacimiento
 		self.edad = edad
-		self.contactos = []
+		self.contactos = [None, None, None]
 		self.nacionalidad = nacionalidad
 
 	def agregar_contacto(self, contacto ):
 		self.contactos.append( contacto )
+
+	def set_telefono_primario(self, telefono):
+		self.contactos[0] = telefono
+
+	def set_telefono_secundario(self, telefono):
+		self.contactos[1] = telefono
+
+	def set_correo(self, correo):
+		self.contactos[2] = correo
+
+	def get_telefono_primario(self):
+		return self.contactos[0]
+
+	def get_telefono_secundario(self):
+		return self.contactos[1]
+
+	def get_correo(self):
+		return self.contactos[2]
 
 	def eliminar_contacto(self, posicion_contacto ):
 		self.contactos.pop( posicion_contacto )
@@ -59,7 +77,8 @@ class Persona:
 		return self.edad
 
 	def __str__(self):
-		return "Nombre: {} Apellido: {} Nacionalidad: {}".format(self.nombre, self.apellido, self.nacionalidad.get_nombre_pais())
+		return "Nombre:{} Apellido:{} Edad:{} Nacionalidad:{} tel1:{} tel2:{} email: {}".format(self.nombre, self.apellido, self.edad,
+											self.nacionalidad.get_nombre_pais(), self.contactos[0], self.contactos[1], self.contactos[2])
 
 	def get_contactos(self):
 		return self.contactos
